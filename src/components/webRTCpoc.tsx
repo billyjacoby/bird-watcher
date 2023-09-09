372540633511d46e7c31730a6054e016585e1e01
Billy Jacoby <billyjacoby@gmail.com>
2023-09-08 21:29:47 -0400

    feat: cleanup webrtc code

--- src/components/webRTCpoc.tsx
+++ src/components/webRTCpoc.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
-import {Text} from 'react-native';
+import {ActivityIndicator, Text, TouchableOpacity} from 'react-native';
 
 import {MediaStream, RTCPeerConnection, RTCView} from 'react-native-webrtc';
 
+import {BaseText} from './baseText';
+import {BaseView} from './baseView';
 import {API_BASE} from '@env';
 
 const webRTCconfig = {
@@ -14,12 +15,15 @@ interface WebRTCPocProps {
   cameraName?: string;
 }
 
+const MAX_RETRIES = 10;
+
 export const WebRTCPOC = ({cameraName}: WebRTCPocProps) => {
   const cameraURL =
     API_BASE.replace('http', 'ws') + '/live/webrtc/api/ws?src=' + cameraName;
 
-  const [reload, setReload] = React.useState<boolean>(false);
-  const [loading, setLoading] = React.useState<boolean>(true);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [retryAttempts, setRetryAttempts] = React.useState(0);
+  const [shouldRetry, setShouldRetry] = React.useState(false);
   const [remoteStream, setRemoteStream] = React.useState<MediaStream | null>(
     null,
   );
@@ -27,81 +31,104 @@ export const WebRTCPOC = ({cameraName}: WebRTCPocProps) => {
     null,
   );
 
-  const pcRef = React.useRef<RTCPeerConnection | null>(null);
-  const wsRef = React.useRef<WebSocket | null>(null);
+  const isError = retryAttempts > MAX_RETRIES;
 
-  function onIceConnect() {
+  const pcRef = React.useRef<RTCPeerConnection>(
+    new RTCPeerConnection(webRTCconfig),
+  );
+  const wsRef = React.useRef<WebSocket>(new WebSocket(cameraURL));
+
+  const onIceConnect = () => {
     if (pcRef?.current?.iceConnectionState === 'connected') {
-      setLoading(false);
+      setIsLoading(false);
     }
-  }
+  };
 
-  const connect = React.useCallback(async (url: string) => {
-    setLoading(true);
-    pcRef.current = new RTCPeerConnection(webRTCconfig);
-    const pc = pcRef.current;
-    // add this before connecting to websocket to ensure it is always captured.
-    pcRef.current.addEventListener('iceconnectionstatechange', onIceConnect);
+  const onTrack = async (event: any) => {
+    // Grab the remote track from the connected participant.
+    const track = event?.track;
+    if (track) {
+      const remoteMediaStream = new MediaStream(undefined);
+      remoteMediaStream.addTrack(track);
+      setRemoteStream(remoteMediaStream);
+    }
+  };
 
-    wsRef.current = new WebSocket(url);
-    const ws = wsRef.current;
-
+  const setLocalAvailability = (peerConnection: RTCPeerConnection) => {
+    //? This sets the tracks on the local device, should before anything else i think
     const tracks = [
-      pc.addTransceiver('video', {
+      peerConnection.addTransceiver('video', {
         direction: 'recvonly',
         // codecs: ['H264'],
       }).receiver.track,
-      pc.addTransceiver('audio', {
+      peerConnection.addTransceiver('audio', {
         direction: 'recvonly',
       }).receiver.track,
     ];
 
     setLocalStream(new MediaStream(tracks));
+  };
 
-    const remoteMediaStream = new MediaStream(undefined);
+  const onIceCandidate = (ev: any) => {
+    if (ev.candidate === null) {
+      // Gathering is complete?
+    }
+    //? This is where we send the new icecandidate info to the server
+    if (!ev.candidate) {
+      return;
+    }
 
-    pc.addEventListener('track', async (event: any) => {
-      // Grab the remote track from the connected participant.
-      const track = event?.track;
-      if (track) {
-        remoteMediaStream.addTrack(event.track);
-        setRemoteStream(remoteMediaStream);
-      }
+    const msg = {
+      type: 'webrtc/candidate',
+      value: ev.candidate.candidate,
+    };
+
+    wsRef.current.send(JSON.stringify(msg));
+  };
+
+  //? WS Handlers
+  const onWsOpen = async (pc: RTCPeerConnection, ws: WebSocket) => {
+    //? Websocket is how we're handling sdp negotiation with the frigate server
+    //? Creating the offer is what triggers the gathering of icecandidates
+    const offer = await pc.createOffer({});
+    await pc.setLocalDescription(offer);
+
+    if (pc.localDescription) {
+      const msg = {type: 'webrtc/offer', value: pc.localDescription.sdp};
+      ws.send(JSON.stringify(msg));
+    }
+  };
+
+  const onWsMessage = async (ev: any, pc: RTCPeerConnection) => {
+    const msg = JSON.parse(ev.data);
+    if (msg.type === 'webrtc/candidate') {
+      pc.addIceCandidate(msg.value);
+    } else if (msg.type === 'webrtc/answer') {
+      pc.setRemoteDescription({type: 'answer', sdp: msg.value});
+    }
+  };
+
+  const connect = React.useCallback(async (url: string) => {
+    setIsLoading(true);
+    pcRef.current = new RTCPeerConnection(webRTCconfig);
+    const pc = pcRef.current;
+
+    wsRef.current = new WebSocket(url);
+    const ws = wsRef.current;
+
+    pc.addEventListener('iceconnectionstatechange', onIceConnect);
+
+    setLocalAvailability(pc);
+    //? Add our local tracks (recieve only)
+    pc.addEventListener('track', onTrack);
+
+    ws.addEventListener('open', () => {
+      pc.addEventListener('icecandidate', onIceCandidate);
+      //? Broadcast icecandidates to server
+      onWsOpen(pc, ws);
     });
 
-    ws.addEventListener('open', async () => {
-      pc.addEventListener('icecandidate', (ev: any) => {
-        //? This is where we send the new icecandidate info to the server
-        if (!ev.candidate) {
-          return;
-        }
-
-        const msg = {
-          type: 'webrtc/candidate',
-          value: ev.candidate.candidate,
-        };
-
-        ws.send(JSON.stringify(msg));
-      });
-
-      const offer = await pc.createOffer({});
-      await pc.setLocalDescription(offer);
-
-      if (pc.localDescription) {
-        //? Sending our offer to the server
-        const msg = {type: 'webrtc/offer', value: pc.localDescription.sdp};
-        ws.send(JSON.stringify(msg));
-      }
-    });
-
-    ws.addEventListener('message', async ev => {
-      const msg = JSON.parse(ev.data);
-      if (msg.type === 'webrtc/candidate') {
-        pc.addIceCandidate(msg.value);
-      } else if (msg.type === 'webrtc/answer') {
-        pc.setRemoteDescription({type: 'answer', sdp: msg.value});
-      }
-    });
+    ws.addEventListener('message', ev => onWsMessage(ev, pc));
 
     // the following is to ensure that ice connection eventually becomes connected or completed
     // as it can get stuck in other states which are an error state
@@ -115,43 +142,73 @@ export const WebRTCPOC = ({cameraName}: WebRTCPocProps) => {
       attempts++;
 
       if (attempts > 5) {
+        setRetryAttempts(prev => prev + 1);
         throw new Error('Could not connect');
       }
     }
+    setRetryAttempts(0);
 
     // we no longer want to listen to connected state change events
     pc.removeEventListener('iceconnectedstatechange', onIceConnect);
+    pc.removeEventListener('track', onTrack);
   }, []);
 
   React.useEffect(() => {
     if (cameraURL && cameraName) {
       connect(cameraURL).catch(() => {
-        // toggle the reload value on error so this useEffect will run again
-        setReload(!reload);
+        if (retryAttempts < MAX_RETRIES) {
+          setShouldRetry(prev => !prev);
+        }
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cameraURL, cameraName, shouldRetry]);
 
-    return () => {
-      remoteStream?.getTracks().forEach(t => t.stop());
-      remoteStream?.release(true);
-      localStream?.getTracks().forEach(t => t.stop());
-      localStream?.release();
-      pcRef?.current?.close();
-    };
-  }, [cameraURL, cameraName, reload]);
+  React.useEffect(() => {
+    //? When the camera changes we want to close out the open streams.
+    remoteStream?.getTracks().forEach(t => t.stop());
+    remoteStream?.release(true);
+    localStream?.getTracks().forEach(t => t.stop());
+    localStream?.release();
+  }, [cameraURL]);
 
   if (!cameraName) {
     return null;
   }
 
-  if (loading) {
-    return <Text>Loading ...</Text>;
+  if (isError) {
+    return (
+      <BaseView className="flex-1 mt-4 px-6">
+        <BaseText className="text-center mb-2 text-red-600 text-lg">
+          Unable to load streme for {cameraName}
+        </BaseText>
+        <TouchableOpacity
+          className="self-center border border-red-700 p-3 px-6 rounded-md"
+          onPress={() => setRetryAttempts(0)}>
+          <BaseText>Try Again</BaseText>
+        </TouchableOpacity>
+      </BaseView>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <BaseView className="flex-1 justify-center">
+        <BaseText className="text-center mb-2">
+          Loading stream for camera: {cameraName}
+        </BaseText>
+        <ActivityIndicator size={'large'} />
+        <BaseText className="text-center mt-2">
+          Retry attempts: {retryAttempts}
+        </BaseText>
+      </BaseView>
+    );
   }
 
   if (remoteStream) {
     return (
       <RTCView
-        style={{width: '100%', height: '100%'}}
+        className="w-full h-full"
         objectFit={'contain'}
         streamURL={remoteStream.toURL()}
       />
